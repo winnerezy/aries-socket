1d9049dbf52ad3df83590eecc44d1fc0b756d4d0
Winnerezy <eragbonikpeya@gmail.com>
2024-07-01 15:30:41 +0100

    Fixing CORS

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "https://aries-three.vercel.app"],
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
